23633fe8563b1cfbb2aedc8887e2b645511e5132
Solk32 <nesolk13@gmail.com>
2023-10-31 20:05:58 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	todo "REST_Api"
+	"REST_Api/pkg/handler"
+	"REST_Api/pkg/repository"
+	"REST_Api/pkg/service"
+	"log"
+)
+
+func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
