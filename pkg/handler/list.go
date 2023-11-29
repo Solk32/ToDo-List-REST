12e3ee9248463261d43fa9c43ba7e3d36021e886
Solk32 <nesolk13@gmail.com>
2023-11-29 19:20:52 +0300

    create db logic join to proj

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -4,6 +4,7 @@ import (
 	todo "REST_Api"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func (h *Handler) createList(c *gin.Context) {
@@ -51,7 +52,22 @@ func (h *Handler) getAllLists(c *gin.Context) {
 }
 
 func (h *Handler) getListById(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	lists, err := h.services.TodoList.GetAll(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+
+	c.JSON(http.StatusOK, getAllListsResponse{
+		Data: lists,
+	})
 }
 
 func (h *Handler) updateList(c *gin.Context) {
